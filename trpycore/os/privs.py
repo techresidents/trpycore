82ddc36ccf60fe900d71ea231833738aecd71bf5
Jeffrey Mullins <jmullins@30and30.com>
2012-03-28 17:26:40 -0400

    Adding utilities for dropping privileges.

---
+++ trpycore/os/privs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import grp
+import os
+import pwd
+
+def drop_privs(username, groupname, umask=None):
+
+    #If not root, nothing to drop.
+    if os.getuid() != 0:
+        return
+
+    uid = pwd.getpwnam(username).pw_uid
+    gid = grp.getgrnam(groupname).gr_gid
+
+    #Remove groups
+    os.setgroups([])
+
+    #Set gid and uid
+    os.setgid(gid)
+    os.setuid(uid)
+    
+    #Set umask if provided
+    if umask is not None:
+        os.umask(umask)
