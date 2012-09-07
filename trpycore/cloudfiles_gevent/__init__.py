978c767096a3ab9fcc35493757e632e415bd95ef
Jeffrey Mullins <jmullins@techresidents.com>
2012-09-07 14:04:02 -0400

    Fixing cloudfiles gevent patching.

--- trpycore/cloudfiles_gevent/__init__.py
+++ trpycore/cloudfiles_gevent/__init__.py
@@ -1,9 +1,11 @@
 """Makes Rackspace cloudfiles compatible with gevent by patching socket"""
 
 import gevent
-import gevent.socket
+import gevent.monkey
+
+#unfortunately no better way to patch cloudfiles since it uses httplib.
+gevent.monkey.patch_socket()
+gevent.monkey.patch_httplib()
 
 import cloudfiles
 from cloudfiles import *
-
-cloudfiles.connection.socket = gevent.socket
