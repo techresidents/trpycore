a666af958c9e92d86af93ff82f31e0f6659fbb46
Jeffrey Mullins <jmullins@techresidents.com>
2012-09-07 14:43:06 -0400

    Updating cloudfiles gevent.

--- trpycore/cloudfiles_gevent/__init__.py
+++ trpycore/cloudfiles_gevent/__init__.py
@@ -3,8 +3,14 @@
 import gevent
 import gevent.monkey
 
-#unfortunately no better way to patch cloudfiles since it uses httplib.
+#Unfortunately no better way to patch cloudfiles since it uses httplib.
+
+#Note that as of 1.7.10 cloudfiles library is not properly calling
+#read() on all responses which is causing the next request to 
+#fail with ResponseNotReady exception. As a result, this library
+#is not currently usable with gevent.
 gevent.monkey.patch_socket()
+gevent.monkey.patch_ssl()
 gevent.monkey.patch_httplib()
 
 import cloudfiles
