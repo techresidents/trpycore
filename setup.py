3a261e0309ef912817c18a80fa885215f0e30705
Jeffrey Mullins <jmullins@techresidents.com>
2012-09-20 11:58:49 -0400

    Bumping version to 0.8-SNAPSHOT

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_packages():
 
 setup(
     name='trpycore',
-    version = '0.7.0',
+    version = '0.8-SNAPSHOT',
     author = 'Tech Residents, Inc.',
     packages = find_packages(),
     ext_modules = [atomic_value],
